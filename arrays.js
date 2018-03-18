cc03ab88f073840fedc030003e8337f3cdd32f1b
A MacGregor <annabel.macgregor@gmail.com>
2018-03-18 10:39:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,5 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  [`${element}`, ...array];
+  var chocolateBars = [`${element}`, ...array];
 }
