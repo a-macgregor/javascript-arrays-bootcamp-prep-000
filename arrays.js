43fd6a7e714360ccf5a77794cc7d6dbe74c6cdb7
A MacGregor <annabel.macgregor@gmail.com>
2018-03-18 11:52:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,7 +37,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 //7 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(0);
+  array.slice(1);
   return array;
 }
 
