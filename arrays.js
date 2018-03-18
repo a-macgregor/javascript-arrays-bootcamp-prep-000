8db026548a79673355d2a66aa9f4be938fe0b4da
A MacGregor <annabel.macgregor@gmail.com>
2018-03-18 10:43:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
+//Test 1
 function addElementToBeginningOfArray(array, element) {
   [chocolateBars.unshift(`${element}`)];
 }
 
+//Test 2
 function destructivelyAddElementToBeginningOfArray(array, element) {
   chocolateBars = [`${element}`, ...array];
 }
+
+//Test 3 
